a1f5049d38539610c7dfc8504510b430f43ac2ae
Dennys <dennys1234pelegrin@gmail.com>
2020-02-27 08:04:44 -0500

    Helloasd

---
+++ src/main/java/dennys/iBluPrint/gui/GuiSchematicEdit.java
@@ -0,0 +1,71 @@
+package dennys.iBluPrint.gui;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.resources.I18n;
+import net.minecraftforge.fml.client.config.GuiUnicodeGlyphButton;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class GuiSchematicEdit extends GuiScreen{
+	private final GuiScreen lastScreen;
+	private static final Logger LOGGER = LogManager.getLogger("it");
+	
+	public GuiSchematicEdit(final GuiScreen guiScreen) {
+		this.lastScreen = guiScreen;
+	}
+	
+	@Override
+	public void initGui() {
+		this.buttonList.clear();
+		int id = 0;
+		
+		if (this.mc.world != null) {
+			this.buttonList.add(new GuiNumberField(this.fontRenderer, id++, this.width/2 - 50, this.height/2 - 30, 100, 20));
+			this.buttonList.add(new GuiNumberField(this.fontRenderer, id++, this.width/2 - 50, this.height/2 - 5, 100, 20));
+			this.buttonList.add(new GuiNumberField(this.fontRenderer, id++, this.width/2 - 50, this.height/2 + 20, 100, 20));
+			this.buttonList.add(new GuiButton(id++, this.width - 90, this.height - 200, 80, 20, I18n.format("Unload")));
+			this.buttonList.add(new GuiButton(id++, this.width - 90, this.height - 190, 80, 20, I18n.format("Move Here")));
+			this.buttonList.add(new GuiUnicodeGlyphButton(id++, this.width - 90, this.height - 180, 80, 20, " " + I18n.format("Rotate"), "\u21bb", 2.0f));
+			this.buttonList.add(new GuiButton(id++, 10, this.height - 70, 80, 20, I18n.format("Materials")));
+			
+		}
+	}
+	
+	@Override
+	public void updateScreen() {
+		for (GuiButton button : this.buttonList) {
+			if (button instanceof GuiNumberField) {
+				((GuiNumberField) button).updateCursorCounter();
+			}
+		}
+		super.updateScreen();
+	}
+	
+	@Override
+	public void mouseClicked(int x, int y, int mouseButton) throws IOException{
+		for (GuiButton button : this.buttonList) {
+			if (button instanceof GuiNumberField) {
+				((GuiNumberField) button).mouseClicked(x,  y,  mouseButton);
+			}
+		}
+		super.mouseClicked(x,  y,  mouseButton);
+	}
+	
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks){
+		drawCenteredString(this.fontRenderer, I18n.format("Move Schematic"), this.width/2, this.height/2 - 45, 0xFFFFFF);
+        drawString(this.fontRenderer, I18n.format("X: "), this.width/2 - 65, this.height/2 - 24, 0xFFFFFF);
+        drawString(this.fontRenderer, I18n.format("Y: "), this.width/2 - 65, this.height/2 + 1, 0xFFFFFF);
+        drawString(this.fontRenderer, I18n.format("Z: "), this.width/2 - 65, this.height/2 + 26, 0xFFFFFF);
+
+        super.drawScreen(mouseX, mouseY, partialTicks);
+	}
+}
--- src/main/java/dennys/iBluPrint/proxy/CommonProxy.java
+++ src/main/java/dennys/iBluPrint/proxy/CommonProxy.java
@@ -1,8 +1,22 @@
 package dennys.iBluPrint.proxy;
 
+import dennys.iBluPrint.util.handlers.InputHandler;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class CommonProxy {
+	
+	public void preInit(FMLPreInitializationEvent event) {
+		ClientRegistry.registerKeyBinding(InputHandler.areaSelection);
+	}
+	
+	public void init(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(InputHandler.INSTANCE);
+	}
+	
 	public void registerItemRenderer(Item item, int meta, String id) {
 		
 	}
